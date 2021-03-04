dabe52b7b5e14bc6eb052166dd666673c2a542d9
coderprasukj <prasukjain0047@gmail.com>
2021-03-05 00:01:52 +0530

    Login Page Midway

--- src/components/LoginPageComponent.js
+++ src/components/LoginPageComponent.js
@@ -1,39 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import {
-  Button,
   Container,
   IconButton,
   makeStyles,
   TextField,
   withStyles,
 } from "@material-ui/core";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import "../styles/LoginPageComponent.css";
-import { AccountCircle } from "@material-ui/icons";
+import {
+  AccountCircle,
+  Language,
+  Visibility,
+  VisibilityOff,
+} from "@material-ui/icons";
 import themeColor from "./Apptheme";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  emailbox: {
+    width: "73vw",
+    marginLeft: theme.spacing(2),
     display: "flex",
-    flexWrap: "wrap",
   },
-  margin: {
+  otherbox: {
+    width: "73vw",
     marginTop: theme.spacing(2),
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "50%",
-  },
-  textField: {
-    width: "40ch",
-    "& .MuiOutlinedInput-root": {
-      "&:hover fieldset": {
-        borderColor: themeColor[0],
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: themeColor[0],
-      },
-    },
+    marginLeft: theme.spacing(2),
   },
 }));
 
@@ -43,6 +34,9 @@ const CssTextField = withStyles({
       color: themeColor[0],
     },
     "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: themeColor[1],
+      },
       "&:hover fieldset": {
         borderColor: themeColor[0],
       },
@@ -55,14 +49,17 @@ const CssTextField = withStyles({
 
 export const LoginPageComponent = () => {
   const classes = useStyles();
-  const [values, setValues] = React.useState({
+  const [values, setValues] = useState({
     email: "",
     password: "",
-    country: "",
+    timezone: "",
     showPassword: false,
   });
 
+  const [signup, setSignup] = useState(false);
+
   const handleChange = (prop) => (event) => {
+    console.log(prop);
     setValues({ ...values, [prop]: event.target.value });
   };
 
@@ -78,50 +75,113 @@ export const LoginPageComponent = () => {
     <Fragment>
       <Container className="login_wave">
         <img src="./assets/full-logo.png" alt="logo" />
+        <h2 className="login_heading">Welcome Coder!!!</h2>
       </Container>
       <Container className="login_container">
-        <h2 className="login_heading">Welcome Coder!!!</h2>
         <Container>
-          <form noValidate>
-            <CssTextField
-              className={classes.margin}
-              label="Email ID"
-              // value={values.email}
-              variant="outlined"
-              labelWidth={60}
-              required
-              InputProps={{
-                endAdornment: (
-                  <IconButton>
-                    <AccountCircle />
-                  </IconButton>
-                ),
-              }}
-            />
+          <form noValidate className="login_form">
+            <Container>
+              <CssTextField
+                className={classes.emailbox}
+                label="Email ID"
+                autoComplete="email"
+                value={values.email}
+                onChange={handleChange("email")}
+                variant="outlined"
+                labelWidth={60}
+                required
+                InputProps={{
+                  endAdornment: (
+                    <IconButton edge="end">
+                      <AccountCircle style={{ color: themeColor[1] }} />
+                    </IconButton>
+                  ),
+                }}
+              />
+            </Container>
 
-            <CssTextField
-              className={classes.margin}
-              label="Password"
-              variant="outlined"
-              labelWidth={60}
-              type={values.showPassword ? "text" : "password"}
-              required
-              InputProps={{
-                endAdornment: (
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                ),
-              }}
-            />
+            <Container>
+              <CssTextField
+                className={classes.otherbox}
+                label="Password"
+                autoComplete="current-password"
+                value={values.password}
+                onChange={handleChange("password")}
+                variant="outlined"
+                labelWidth={60}
+                type={values.showPassword ? "text" : "password"}
+                required
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? (
+                        <Visibility style={{ color: themeColor[1] }} />
+                      ) : (
+                        <VisibilityOff style={{ color: themeColor[1] }} />
+                      )}
+                    </IconButton>
+                  ),
+                }}
+              />
+            </Container>
+            {signup && (
+              <Container>
+                <CssTextField
+                  className={classes.otherbox}
+                  label="TimeZone"
+                  onChange={handleChange("timezone")}
+                  variant="outlined"
+                  labelWidth={60}
+                  required
+                  InputProps={{
+                    endAdornment: (
+                      <IconButton edge="end">
+                        <Language style={{ color: themeColor[1] }} />
+                      </IconButton>
+                    ),
+                  }}
+                />
+              </Container>
+            )}
           </form>
         </Container>
-        <Button variant="contained">Login</Button>
+        <Container className="login_buttonContainer">
+          {!signup && (
+            <button
+              className="login_button"
+              onClick={() => alert(JSON.stringify(values))}
+            >
+              <h2>LogIn</h2>
+            </button>
+          )}
+          {!signup && <h3 className="login_or">--------OR--------</h3>}
+          {!signup && (
+            <button
+              className="signup_button"
+              onClick={() => setSignup(!signup)}
+            >
+              <h2>SignUp</h2>
+            </button>
+          )}
+          {signup && (
+            <button
+              className="signup_button"
+              onClick={() => alert(JSON.stringify(values))}
+            >
+              <h2>Done!!!</h2>
+            </button>
+          )}
+          {signup && (
+            <h5 className="login_goback" onClick={() => setSignup(!signup)}>
+              Go Back to LogIn
+            </h5>
+          )}
+        </Container>
       </Container>
     </Fragment>
   );
